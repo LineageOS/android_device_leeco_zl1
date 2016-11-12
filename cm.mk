07d38ce051c9838f3d98ec2d0ca1da39e2266590
Sean Hoyt <seanhoyt963@gmail.com>
2016-11-11 21:32:20 -0600

    Remove nfc support

--- cm.mk
+++ cm.mk
@@ -19,9 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/leeco/x2/device.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
