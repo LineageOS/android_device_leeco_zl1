ea4eb1f09b21aa9611aabc27fbc16e4f0bf03b18
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-02-07 19:38:52 +0100

    zl1: lineage rebrand
    Change-Id: I5c339c9bcbc42dc110e6f2e7749f8433ab977a3b

--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,10 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/leeco/zl1/device.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_zl1
+PRODUCT_NAME := lineage_zl1
 PRODUCT_DEVICE := zl1
 PRODUCT_MANUFACTURER := LeMobile
 PRODUCT_BRAND := LeEco
