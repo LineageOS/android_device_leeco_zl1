8a82ff5344aefe5ed935bd00398e7b6e0b1d1a03
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-02-08 21:14:40 +0100

    zl1: lights: remove unneeded scaling of bl brightness
    Change-Id: Ib2184fdf4959385608cd4997ba754923cf5830bf

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,15 +14,13 @@
 # limitations under the License.
 
 LOCAL_PATH:= $(call my-dir)
-# HAL module implemenation stored in
-# hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := lights.c
-LOCAL_MODULE_RELATIVE_PATH    := hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 
-LOCAL_MODULE := lights.msm8996
+LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
