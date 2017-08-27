81a1594f9456d62515d585d819aedc3948ff864f
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-08-27 20:52:58 +0200

    zl1: don't build lights HAL
    Change-Id: If3d388fb6e55e902be1d7d5400b4898a56365db0

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -13,14 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
+#LOCAL_PATH:= $(call my-dir)
+#include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := lights.c
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils
+#LOCAL_SRC_FILES := lights.c
+#LOCAL_MODULE_RELATIVE_PATH := hw
+#LOCAL_SHARED_LIBRARIES := liblog libcutils
 
-LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
+#LOCAL_MODULE_TAGS := optional
 
-include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_SHARED_LIBRARY)
