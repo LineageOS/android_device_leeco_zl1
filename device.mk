9385ea70bbef92a4568238ba92e247d57aa816ff
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-02 23:10:33 +0100

    zl1: enable lineage overlays
    Change-Id: I4535c4bb0b20ef590e65fee161e34786d9b4187d

--- device.mk
+++ device.mk
@@ -25,8 +25,8 @@ $(call inherit-product-if-exists, vendor/leeco/zl1/zl1-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
-#    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
