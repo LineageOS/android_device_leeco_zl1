a548263db7a474f00a8036f67455c8c72180b83a
TheScarastic <warabhishek@gmail.com>
2018-03-08 20:15:45 +0530

    zl1: biometrics: update for P
    Change-Id: I130babba067d3d1815f1d13b29a454a29bd3a2ea

--- biometrics/Android.mk
+++ biometrics/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.0-service.leeco_zl1
-LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.0-service.rc
+LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.1-service.leeco_zl1
+LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.1-service.leeco_zl1.rc
 LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_VENDOR_MODULE := true
@@ -26,7 +26,6 @@ LOCAL_SHARED_LIBRARIES := \
     libutils \
     libhwbinder \
     libkeystore_aidl \
-    libkeystore_binder \
     android.hardware.biometrics.fingerprint@2.1
 
 include $(BUILD_EXECUTABLE)
