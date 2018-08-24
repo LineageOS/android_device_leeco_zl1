3320e5c70312a4ee576e2e0c5c4013662180735d
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-08-24 18:05:31 +0200

    zl1: go treble
    Change-Id: I9bc4b3f745fb17165945ca43cd06054acbd1dc6e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,7 @@ TARGET_OTA_ASSERT_DEVICE := le_zl0,le_zl1,LEX720,LEX722,LEX727,zl0,zl1
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
 BOARD_KERNEL_CMDLINE += androidboot.configfs=true
-
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 
@@ -51,15 +51,17 @@ BOARD_NFC_HAL_SUFFIX := msm8996
 # Partitions
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4294967296
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 26144878592
+BOARD_VENDORIMAGE_PARTITION_SIZE := 649523200
 
 # Lineage Hardware
 BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
 
 # Shims
-TARGET_LD_SHIM_LIBS += :/system/lib64/hw/gxfingerprint5118m.default.so|fakelogprint.so:/system/lib64/hw/fingerprint.vendor.msm8996.so|fakelogprint.so:/system/bin/gx_fpd|fakelogprint.so
+TARGET_LD_SHIM_LIBS += :/vendor/lib64/hw/gxfingerprint5118m.default.so|fakelogprint.so:/vendor/lib64/hw/fingerprint.vendor.msm8996.so|fakelogprint.so:/vendor/bin/gx_fpd|fakelogprint.so
 
 # SELinux
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # inherit from the proprietary version
 -include vendor/leeco/zl1/BoardConfigVendor.mk
