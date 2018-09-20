d28690a1cb590de39c9d876076079423e243eca7
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-09-20 06:33:58 +0200

    zl1: Set vendor props via PRODUCT_PROPERTY_OVERRIDES
    Change-Id: I7513f50f47520b8372a9881eabcf2f45459211b2

---
+++ vendor_prop.mk
@@ -0,0 +1,22 @@
+# Product
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.product=le_zl1 \
+    ro.config.le_hardware_version=HW_1.0.0 \
+    ro.display.series="Le Pro3"
+
+# Apical AD
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qcom.ad.calib.data=/vendor/etc/ad_calib_le_zl1.cfg
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=420
+
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.nfc_nci=nqx.default \
+    ro.nfc.port=I2C
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.qfp=false
