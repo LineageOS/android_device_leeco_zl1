28e2f57f6908a2fb9aac29ce7e2ac697be14b108
Abhishek Aggarwal <warabhishek@gmail.com>
2018-11-30 00:02:34 -0700

    zl1: add thermal-engine.conf from 28S
    Change-Id: I7bee6e25ba88b25f0a23634321009f2c0a72af27

--- device.mk
+++ device.mk
@@ -102,6 +102,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+# Thermal-engine
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
