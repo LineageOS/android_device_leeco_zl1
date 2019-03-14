d7fcc453b200f421ab508978f7934afcf109d045
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-03-14 17:13:13 +0100

    zl1: update nfc configs to match upstream
     * Match all the changes up to LA.UM.7.5.r1-04300-8x96.0
    
    libnfc-nxp.conf <= libnfc-nxp-PN66T_example.conf
    
    Change-Id: I1fdb487024ee0726cbf74e654c5fd6af39ad6ff5

--- device.mk
+++ device.mk
@@ -75,6 +75,7 @@ PRODUCT_PACKAGES += \
     libnqnfc-nci \
     libnqp61-jcop-kit \
     nfc_nci.nqx.default \
+    nfc_nci.nqx.default.hw \
     NQNfcNci \
     nqnfcee_access.xml \
     nqnfcse_access.xml \
@@ -83,8 +84,7 @@ PRODUCT_PACKAGES += \
     vendor.nxp.hardware.nfc@1.1-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Permissions
