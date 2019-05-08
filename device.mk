7a1dcc8e6efbccb549b59209c86cea6c7ba3dae7
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2019-05-08 17:57:42 +0200

    Revert "zl1: Switch to NQ NFC stack"
    This reverts commit 698e18d70635e4830cb6616de12f7621535cb432.
    
    Reason for revert: still broken!
    
    Change-Id: I35ff34b1c678beb8e8afaaaf614c5b338e3536f3

--- device.mk
+++ device.mk
@@ -68,20 +68,13 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.1-service \
     com.android.nfc_extras \
-    com.nxp.nfc.nq \
-    com.nxp.nfc.nq.xml \
-    libnqnfc-nci \
-    libnqnfc_nci_jni \
-    libp61-jcop-kit \
-    nfc_nci.nqx.default \
-    nfc_nci.nqx.default.hw \
-    NQNfcNci \
+    NfcNci \
     nqnfcee_access.xml \
     nqnfcse_access.xml \
     SecureElement \
-    Tag \
-    vendor.nxp.hardware.nfc@1.1-service
+    Tag
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
